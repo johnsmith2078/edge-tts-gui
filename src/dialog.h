abfa9922c39782488056072238617b68a08a52d8
johnsmith2078 <160999470+johnsmith2078@users.noreply.github.com>
2026-09-06 01:40:55 +0800

    chore: make dialog file-reading dependencies explicit

--- src/dialog.h
+++ src/dialog.h
@@ -2,8 +2,11 @@
 #define DIALOG_H
 
 #include <QDialog>
+#include <QFile>
 #include <QKeyEvent>
 #include <QMap>
+#include <QTextStream>
+
 #include "communicate.h"
 #include "streamingprogressbar.h"
 
@@ -20,54 +23,54 @@ class Dialog : public QDialog
     Q_OBJECT
 
 public:
-    static Dialog& getInstance() {
+    static Dialog &getInstance()
+    {
         static Dialog instance {};
         return instance;
     }
 
-
-private:
-    Dialog(QWidget *parent = nullptr);
-public:
     ~Dialog();
+
+    void playText(const QString &text);
+    void stopPlayback();
+    bool isPlaybackActive() const;
+    void setManuallyStopped(bool manuallyStopped);
 
 signals:
     void send();
-
     void stop();
-
     void playbackActiveChanged(bool active);
-
-private:
-    void checkDuplicate(const QString& text, const QString& voice);
-
-    void setCommunicate(const QString& text, const QString& voice, const QString& fileName);
-
-public:
-    void playText(const QString& text);
-
-    void stopPlayback();
-
-    bool isPlaybackActive() const;
 
 private slots:
     void on_pushButtonPlay_clicked();
-
     void on_radioButtonXiaoxiao_clicked(bool checked);
-
     void on_radioButtonXiaoyi_clicked(bool checked);
-
     void on_radioButtonYunjian_clicked(bool checked);
-
     void on_radioButtonYunxi_clicked(bool checked);
-
     void on_radioButtonYunxia_clicked(bool checked);
-
     void on_radioButtonYunyang_clicked(bool checked);
-
     void on_pushButtonStop_clicked();
-
     void on_pushButtonSave_clicked();
+    void onLanguageChanged(const QString &language);
+    void onVoiceNameChanged(const QString &voiceName);
+    void onPlayFinished();
+
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
+    void dragEnterEvent(QDragEnterEvent *event) override;
+    void dropEvent(QDropEvent *event) override;
+
+private:
+    explicit Dialog(QWidget *parent = nullptr);
+
+    void checkDuplicate(const QString &text, const QString &voice);
+    void setCommunicate(const QString &text, const QString &voice, const QString &fileName);
+    void loadVoiceData();
+    void setPlaybackActive(bool active);
+    void updateTextCount();
+    void startAutoRetryAttempt();
+    void handleAutoRetryFinished();
+    void scheduleNoPlaybackWatchdog(int attemptSerial, qsizetype lastEdgeBytesReceived);
 
 private:
     Ui::Dialog *ui;
@@ -75,7 +78,7 @@ private:
     QString m_lastText;
     QString m_lastVoice;
     QString voice;
-    QString lastDir = "";
+    QString lastDir;
 
     bool manuallyStopped = true;
     QString m_autoRetryText;
@@ -87,38 +90,7 @@ private:
     bool m_savingAudio = false;
     QPropertyAnimation *m_saveProgressAnimation = nullptr;
 
-public:
-    void setManuallyStopped(bool manuallyStopped);
-
-private:
-    void loadVoiceData();
-
-    void setPlaybackActive(bool active);
-
-    void updateTextCount();
-
-    QMap<QString, QMap<QString, QString>> data; // 存储语言、语音名称和代码
-
-private slots:
-    void onLanguageChanged(const QString &language);
-
-    void onVoiceNameChanged(const QString &voiceName);
-
-    void onPlayFinished();
-
-protected:
-    bool eventFilter(QObject *obj, QEvent *event) override;
-
-    void dragEnterEvent(QDragEnterEvent *event) override;
-
-    void dropEvent(QDropEvent *e) override;
-
-private:
-    void startAutoRetryAttempt();
-
-    void handleAutoRetryFinished();
-
-    void scheduleNoPlaybackWatchdog(int attemptSerial, qsizetype lastEdgeBytesReceived);
-}; // class Dialog
+    QMap<QString, QMap<QString, QString>> data;
+};
 
 #endif // DIALOG_H
