192b812eb28be172d52beaffd0a14d795402acca
johnsmith2078 <160999470+johnsmith2078@users.noreply.github.com>
2026-09-06 01:35:35 +0800

    fix: harden TTS session error and save state

--- src/communicate.h
+++ src/communicate.h
@@ -1,27 +1,22 @@
 #ifndef COMMUNICATE_H
 #define COMMUNICATE_H
 
-#include <QDir>
-#include <QUuid>
-#include <QFile>
-#include <QFileInfo>
-#include <QProcess>
-#include <QWebSocket>
-#include <QSystemTrayIcon>
-#include <QDesktopServices>
-#include <QtMultimedia/QMediaPlayer>
-#include <QtMultimedia/QAudioOutput>
 #include <QBuffer>
+#include <QMap>
 #include <QQueue>
 #include <QVector>
+#include <QWebSocket>
+#include <QtMultimedia/QAudioOutput>
+#include <QtMultimedia/QMediaPlayer>
 
 class Communicate : public QObject
 {
     Q_OBJECT
 
 public:
-    Communicate(QObject *parent = nullptr);
-    ~Communicate();
+    explicit Communicate(QObject *parent = nullptr);
+    ~Communicate() override;
+
     void save();
     void play();
     void forcePlay();
@@ -29,7 +24,8 @@ public:
     void setVoice(QString voice);
     void setFileName(QString fileName);
     void setDuplicated(bool dup);
-    bool isPlaying();
+
+    bool isPlaying() const;
     bool hasPlaybackStarted() const;
     bool isSynthesisComplete() const;
     qsizetype audioBytesReceived() const;
@@ -49,10 +45,35 @@ signals:
     void finished();
     void stop();
     void saveFinished();
+    void saveFailed(const QString &error);
     void saveProgressChanged(int percent);
     void audioDataReceived();
-    void duplicated();
     void playbackStarted();
+
+private:
+    QString connect_id() const;
+    QString date_to_string() const;
+    QString escape(QString data) const;
+    QString remove_incompatible_characters(QString str) const;
+    QString mkssml(const QString &text, const QString &voice, const QString &rate,
+                   const QString &volume, const QString &pitch) const;
+    QString ssml_headers_plus_data(const QString &requestId, const QString &timestamp,
+                                   const QString &ssml) const;
+    bool get_headers_and_data(const QString &message, QMap<QString, QString> &parameters,
+                              QString &data) const;
+    QString generateSecMsGecToken() const;
+    QString generateSecMsGecVersion() const;
+    QString generateMuid() const;
+    int findSafeUtf8SplitPoint(const QByteArray &text, int limit) const;
+    int adjustSplitPointForXmlEntity(const QByteArray &text, int splitAt) const;
+    QVector<QString> splitTextByByteLength(const QString &text, int byteLength) const;
+    QVector<QString> splitTextForPlayback(const QString &text, int initialByteLength,
+                                          int subsequentByteLength) const;
+
+    void fail(const QString &error);
+    void notifyFinishedOnce();
+    void notifySaveFailedOnce(const QString &error);
+    void tryStartOrContinuePlayback();
 
 private:
     QString m_text;
@@ -61,49 +82,33 @@ private:
     QString m_rate = "+0%";
     QString m_volume = "+0%";
     QString m_pitch = "+0Hz";
+
     QWebSocket m_webSocket;
-    QByteArray m_audioDataReceived = "";
-    bool m_downloadAudio = false;
-    qsizetype m_textPartIndex = 0;
-    QString m_date;
-    bool m_synthesisComplete = false;
-    bool m_stopRequested = false;
-    bool m_playbackErrorOccurred = false;
-    bool m_isDuplicated = false;
-    QMediaPlayer* m_player;
-    QAudioOutput* m_audioOutput;
+    QMediaPlayer *m_player = nullptr;
+    QAudioOutput *m_audioOutput = nullptr;
     QBuffer m_audioBuffer;
-    bool m_playStarted = false;
-    bool m_hasPlaybackStarted = false;
-    qsizetype m_audioOffset = 0;
-    bool m_finishedEmitted = false;
-    bool m_switchingPlaybackSource = false;
+
+    QByteArray m_audioDataReceived;
     QByteArray m_currentTurnAudio;
     QQueue<QByteArray> m_readyPlaybackChunks;
     QVector<QString> m_textParts;
 
-    static const int ms_maxTextByteLength = 4096;
-    static const int ms_initialTextByteLength = 80;
-    static const int ms_targetTextByteLength = 780;
+    bool m_downloadAudio = false;
+    bool m_synthesisComplete = false;
+    bool m_stopRequested = false;
+    bool m_playbackErrorOccurred = false;
+    bool m_playStarted = false;
+    bool m_hasPlaybackStarted = false;
+    bool m_finishedEmitted = false;
+    bool m_saveFailureEmitted = false;
+    bool m_switchingPlaybackSource = false;
+    qsizetype m_textPartIndex = 0;
+    qsizetype m_audioOffset = 0;
+    QString m_date;
 
-private:
-    QString connect_id();
-    QString date_to_string();
-    QString escape(QString data);
-    QString remove_incompatible_characters(QString str);
-    QString mkssml(QString text, QString voice, QString rate, QString volume, QString pitch);
-    QString ssml_headers_plus_data(const QString& requestId, const QString& timestamp, const QString& ssml);
-    QPair<QMap<QString, QString>, QString> get_headers_and_data(const QString& message);
-    void removeTrailingZeros(QByteArray &byteArray);
-    QString generateSecMsGecToken();
-    QString generateSecMsGecVersion();
-    QString generateMuid();
-    int findSafeUtf8SplitPoint(const QByteArray &text, int limit);
-    int adjustSplitPointForXmlEntity(const QByteArray &text, int splitAt);
-    QVector<QString> splitTextByByteLength(const QString &text, int byteLength);
-    QVector<QString> splitTextForPlayback(const QString &text, int initialByteLength, int subsequentByteLength);
-    void notifyFinishedOnce();
-    void tryStartOrContinuePlayback();
+    static constexpr int ms_maxTextByteLength = 4096;
+    static constexpr int ms_initialTextByteLength = 80;
+    static constexpr int ms_targetTextByteLength = 780;
 };
 
 #endif // COMMUNICATE_H
